eda40f6774076821cff7f826d76bb5cc95f6b725
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-07-28 16:17:58 -0700

    Create stat.py

---
+++ stat.py
@@ -0,0 +1,17 @@
+my_name = 'Kendrick O. Ekejiuba'
+my_age = 21
+my_height = 74
+my_weight = 198
+my_eyes = 'Brown'
+my_teeth = 'Off White'
+my_hair = 'Black'
+#using the my functions to create the statistics for your person 
+print(f"Lets talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy")
+print(f"Actually thats not too heavy.")
+print(f"Hes got {my_eyes} eyes and {my_hair} hair.")
+print(f"Hes teeth are usually {my_teeth} depending on the coffee.")
+#implementing the curly brackets
+#The curly brackets are used to determine lists which are the statistics in the program
+# f" Is called a formatted string literals and they go before the curly braces
